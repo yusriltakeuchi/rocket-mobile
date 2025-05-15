94ba695c2cb649f467692e417f9efd4e5a956006
Yusril Rapsanjani <yusriltakeuchi@gmail.com>
2025-05-15 15:48:59 +0700

    Chore: Update connfig

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Panduan Praktis untuk Publish Aplikasi Mobile">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
